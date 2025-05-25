c07b26e1a072df6fa9cda7c3ab7284246459d4ef
Alvix.exe <alvixwick@gmail.com>
2025-05-25 15:40:24 -0400

    Improve the structure of the location where the application is executed.

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from settings import get_parser
 from expense_manager import ExpenseManager
 
-expense = ExpenseManager()
+def main():
+    '''Run the application'''
+    expense = ExpenseManager()
+    args = get_parser()
+    
+    # Here you will control the flow of the CLI application.
+    if args.command == "add":
+        expense.add_expense(args)
+    elif args.command == "list":
+        expense.list_expenses()
+    elif args.command == "delete":
+        expense.delete_expense(args)
+    elif args.command == "update":
+        expense.update_expense(args)
+    elif args.command == "summary":
+        if args.month:
+            expense.summary_expense_for_month(args)
+        else:
+            expense.summary_expense()
 
-args = get_parser()
-
-if args.command == "add":
-    expense.add_expense(args)
-elif args.command == "list":
-    expense.list_expenses()
-elif args.command == "delete":
-    expense.delete_expense(args)
-elif args.command == "summary":
-    if args.month:
-        expense.summary_expense_for_month(args)
-    else:
-        expense.summary_expense()
+if __name__ == "__main__":
+    main()
