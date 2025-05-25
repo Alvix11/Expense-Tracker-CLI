ec32cce2272420ce859f44c2a867f74afb9e2e8b
Alvix.exe <alvixwick@gmail.com>
2025-05-25 13:24:25 -0400

    I modified the logic because it was wrong

--- expense_manager.py
+++ expense_manager.py
@@ -97,14 +97,14 @@ class ExpenseManager:
                      
             summary = sum(amounts)
 
-            for key, value in months.items():
-                if key == int(args.month):
-                    mont = value
-
-                else:
-                    print("Enter a valid month")
-                    return
+            if int(args.month) in months:
+                for key, value in months.items():
                 
-            print(f"Total expenses for {mont}: {summary}")
+                    if key == int(args.month):
+                        mont = value
+                    
+                        print(f"Total expenses for {mont}: {summary}")
+            else:
+                    print("Enter a valid month")
         else:
             print("There are no expenses to summary")
