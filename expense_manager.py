074b15ac1f6f4014b87fc5bd9a85b87b0b90609d
Alvix <alvix.wick@gmail.com>
2025-08-13 23:33:32 -0400

    Deleted unnecessary spaces.

--- expense_manager.py
+++ expense_manager.py
@@ -101,9 +101,9 @@ class ExpenseManager:
         '''Function to summary expense for month'''
         data = load_expense(self.file_path)
         months = {1: 'January', 2: 'February', 3: 'March', 4: 'April',
-                  5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 
-                  10: 'October', 11: 'November', 12: 'December'
-                  } # Dictionary of months
+                5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 
+                10: 'October', 11: 'November', 12: 'December'
+                } # Dictionary of months
 
         if data:
             amounts = []
